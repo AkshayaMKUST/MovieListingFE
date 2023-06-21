0fe5c076184010c745a9295799434ddb7ee829ab
NikhilThomas7 <nikhil.thomas@ust.com>
2023-06-21 17:57:35 +0530

    added the add movie functionality and added the retrieving the fields in update modal func

---
+++ src/app/Model/movietwo.ts
@@ -0,0 +1,13 @@
+export class MovieTwo {
+    movieName: string = '';
+    movieDirector: string = '';
+    movieGenre: string = '';
+    movieReleaseDate: string = '';
+    movieLanguage: string = '';
+    duration: string = '';
+    country: string = '';
+    description: string = '';
+    overallRate: number = 0;
+    imageFile: File | undefined;
+  }
+  
--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Movie } from '../Model/movie';
+import { MovieTwo } from '../Model/movietwo';
 import { ConnectivityService } from '../connectivity.service';
 import { Router } from '@angular/router';
+import {  ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -11,7 +14,9 @@ import { Router } from '@angular/router';
 export class AdminDashboardComponent {
   Movies: Movie[] = [];
   selectedMovie: Movie | undefined;
-
+  movietwo: MovieTwo = new MovieTwo();
+  // isModalOpen: boolean = false;
+  @ViewChild('movieForm', { static: false }) movieForm: ElementRef | undefined;
   constructor(private service: ConnectivityService, private router: Router) {}
 
   ngOnInit(): void {
@@ -35,10 +40,38 @@ export class AdminDashboardComponent {
         console.log('Movie deleted successfully');
         this.getMovies();
         alert('Movie deleted successfully');
-        this.router.navigate(['retrieve']);
+        // this.router.navigate(['retrieve']);
+        // this.isModalOpen = false;
       });
     }
   }
 
-  // Rest of your component code...
+  addMovie() {
+    this.service.saveProduct(this.movietwo).subscribe(
+      () => {
+        console.log('Movie added successfully');
+        alert('Movie added successfully');
+        this.movietwo = new MovieTwo();
+        this.getMovies();
+      },
+      (error) => {
+        console.error('Failed to add movie', error);
+      }
+    );
+  }
+
+  onImageFileChange(event: any) {
+    const file = event.target.files[0];
+    this.movietwo.imageFile = file;
+  }
+
+  resetForm() {
+    if (this.movieForm) {
+      this.movieForm.nativeElement.reset();
+    }
+  }
+
 }
+
+ 
+
