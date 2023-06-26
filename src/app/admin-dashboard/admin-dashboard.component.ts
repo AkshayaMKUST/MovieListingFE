e68f21645447d224631b5050aedeabbbcc0c8efc
NikhilThomas7 <nikhil.thomas@ust.com>
2023-06-26 12:47:56 +0530

    update movie connection added

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -13,13 +13,18 @@ import {  ElementRef } from '@angular/core';
 })
 export class AdminDashboardComponent {
   Movies: Movie[] = [];
-  selectedMovie: Movie | undefined;
+  selectedMovie: any = {};
+  movieId: number = 0;
   movietwo: MovieTwo = new MovieTwo();
+  movie : Movie = new Movie();
   // isModalOpen: boolean = false;
   @ViewChild('movieForm', { static: false }) movieForm: ElementRef | undefined;
-  constructor(private service: ConnectivityService, private router: Router) {}
+  constructor(private service: ConnectivityService, private router: Router) {
+    this.selectedMovie = undefined;
+  }
 
   ngOnInit(): void {
+    // this.selectedMovie = {}
     this.getMovies();
   }
 
@@ -68,8 +73,109 @@ export class AdminDashboardComponent {
   resetForm() {
     if (this.movieForm) {
       this.movieForm.nativeElement.reset();
+      this.selectedMovie = undefined; // Reset selectedMovie to undefined
     }
   }
+
+ 
+
+ updateMovieTwo(): void{
+
+  this.movie.movieName = this.selectedMovie.movieName;
+  this.movie.movieDirector = this.selectedMovie.movieDirector;
+  this.movie.movieGenre = this.selectedMovie.movieGenre;
+  this.movie.movieReleaseDate = this.selectedMovie.movieReleaseDate;
+  this.movie.movieLanguage = this.selectedMovie.movieLanguage;
+  this.movie.duration = this.selectedMovie.duration;
+  this.movie.country = this.selectedMovie.country;
+  this.movie.description = this.selectedMovie.description;
+  this.movie.overallRate = this.selectedMovie.overallRate;
+
+ }
+  
+
+  
+ saveUpdatedProduct(): void {
+  this.movietwo.movieName = this.movie.movieName;
+       this.movietwo.movieDirector = this.movie.movieDirector;
+       this.movietwo.movieGenre = this.movie.movieGenre;
+       this.movietwo.movieReleaseDate = this.movie.movieReleaseDate;
+       this.movietwo.movieLanguage = this.movie.movieLanguage;
+       this.movietwo.duration = this.movie.duration;
+       this.movietwo.country = this.movie.country;
+       this.movietwo.description = this.movie.description;
+       this.movietwo.overallRate = this.movie.overallRate;
+       this.movieId = this.selectedMovie.movieId;
+
+ this.service.updateProduct(this.movietwo,this.movieId).subscribe(
+   (response) => {
+     console.log('Product updated successfully');
+     alert('Movie Updated Successfully');
+     this.getMovies();
+   },
+   (error) => {
+     console.log('Error updating product:', error);
+   }
+ );
+}
+// saveUpdatedProduct(): void {
+//   this.movietwo.movieName = this.selectedMovie.movieName;
+//   this.movietwo.movieDirector = this.movie.movieDirector;
+//   this.movietwo.movieGenre = this.movie.movieGenre;
+//   this.movietwo.movieReleaseDate = this.movie.movieReleaseDate;
+//   this.movietwo.movieLanguage = this.movie.movieLanguage;
+//   this.movietwo.duration = this.movie.duration;
+//   this.movietwo.country = this.movie.country;
+//   this.movietwo.description = this.movie.description;
+//   this.movietwo.overallRate = this.movie.overallRate;
+
+//   if (this.selectedMovie) {
+//     this.movieId = this.selectedMovie.movieId; // Assign selected movie's movieId
+//     this.service.updateProduct(this.movietwo, this.movieId).subscribe(
+//       (response) => {
+//         console.log('Movie updated successfully');
+//         this.getMovies();
+//       },
+//       (error) => {
+//         console.log('Error updating movie:', error);
+//       }
+//     );
+//   }
+// }
+
+
+// saveUpdatedProduct(): void {
+//   if (this.selectedMovie) {
+//     const updatedMovie: MovieTwo = {
+//       movieName:  this.movietwo.movieName,
+//       movieDirector:  this.selectedMovie.movieDirector,
+//       movieGenre:  this.selectedMovie.movieGenre,
+//       movieReleaseDate:  this.selectedMovie.movieReleaseDate,
+//       movieLanguage:  this.selectedMovie.movieLanguage,
+//       duration:  this.selectedMovie.duration,
+//       country:  this.selectedMovie.country,
+//       description:  this.selectedMovie.description,
+//       overallRate:  this.selectedMovie.overallRate,
+//       imageFile: this.movietwo.imageFile
+//     };
+
+//     this.movieId = this.selectedMovie.movieId; // Assign selected movie's movieId
+
+//     this.service.updateProduct(updatedMovie, this.movieId).subscribe(
+//       (response) => {
+//         console.log('Movie updated successfully');
+//         this.getMovies();
+//       },
+//       (error) => {
+//         console.log('Error updating movie:', error);
+//       }
+//     );
+//   }
+// }
+
+
+
+
 
 }
 
--- src/app/connectivity.service.ts
+++ src/app/connectivity.service.ts
@@ -37,4 +37,20 @@ export class ConnectivityService {
     return this.http.post("http://localhost:8083/1.0/admin/addMovie", formData, { responseType: 'text' });
   }
 
+  updateProduct(movie: MovieTwo,id:number): Observable<any> {
+    const formData = new FormData();
+    formData.append('movieName', movie.movieName);
+    formData.append('movieDirector', movie.movieDirector);
+    formData.append('movieGenre', movie.movieGenre);
+    formData.append('movieReleaseDate', movie.movieReleaseDate);
+    formData.append('movieLanguage', movie.movieLanguage);
+    formData.append('duration', movie.duration);
+    formData.append('country', movie.country);
+    formData.append('description', movie.description);
+    formData.append('overallRate', movie.overallRate.toString());
+    formData.append('file', movie.imageFile as Blob);
+  
+    return this.http.put(`http://localhost:8083/1.0/admin/updateAMovie/${id}`, formData, { responseType: 'text' });
+  }
+
 }
