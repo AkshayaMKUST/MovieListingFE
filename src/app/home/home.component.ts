af7372e18e44f59854b45844d6a6a3f969889f94
Akshaya Manha Kunnil(UST <245239@ust.com>
2023-06-26 12:55:06 +0530

    push after frontend branch creation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,26 @@ export class HomeComponent {
   {
     this.router.navigate(['/modal']);
   }
+  selectedOption: string = 'name';
+  searchValue: string = '';
+  searchPlaceholder: string = 'Enter a movie name';
+
+  search() {
+    if (this.selectedOption === 'name') {
+      console.log('Searching by name:', this.searchValue);
+    } else if (this.selectedOption === 'date') {
+      console.log('Searching by date:', this.searchValue);
+    }
+  }
+
+  onSelectedOptionChange() {
+    this.searchValue = ''; // Clear the search field
+    if (this.selectedOption === 'name') {
+      this.searchPlaceholder = 'Enter a movie name';
+    } else if (this.selectedOption === 'date') {
+      this.searchPlaceholder = 'Enter a date';
+    }
+  }
 
 }
 
