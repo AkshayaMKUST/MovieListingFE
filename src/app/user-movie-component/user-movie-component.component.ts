dda9141da257bc562aa633e693a5e4fca506f0bd
Akshaya Manha Kunnil(UST <245239@ust.com>
2023-06-27 14:45:21 +0530

    added change in login and created a rating page

--- src/app/user-movie-component/user-movie-component.component.ts
+++ src/app/user-movie-component/user-movie-component.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-movie-component',
   templateUrl: './user-movie-component.component.html',
   styleUrls: ['./user-movie-component.component.css']
 })
-export class UserMovieComponentComponent {
+export class UserMovieComponentComponent  implements OnInit{
     
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+  reviewmessage:string='';
+  starRating:number=0;
+
+  submitreview(){
+    console.log(this.reviewmessage);
+    console.log(this.starRating);
+  }
   }
   
 
